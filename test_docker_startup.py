016ad9b71ba77c3c88d1da60edaa8fbdb42208f3
singha42 <alka.singh@pnnl.gov>
2020-03-12 14:17:28 -0700

    updated code

--- test_docker_startup.py
+++ test_docker_startup.py
@@ -2,10 +2,13 @@ from contextlib import contextmanager
 import json
 import logging
 import os
-from time import sleep
+from time import sleep,time
 import sys
-
+import pandas as pd
 import pytest
+from numbers import Number
+from math import isclose
+
 
 from gridappsd import GridAPPSD
 # tm: added for run_simulation workaround
@@ -34,12 +37,47 @@ def gappsd() -> GridAPPSD:
     gridappsd.disconnect()
 
 
-def assert_files_are_equal(file1, file2):
-    with open(file1) as f1:
-        with open(file2) as f2:
-            for line_f1 in f1.readline():
-                line_f2 = f2.readline()
-                assert line_f1 == line_f2
+# def assert_files_are_equal(file1, file2):
+#     with open(file1) as f1:
+#         with open(file2) as f2:
+#             for line_f1 in f1.readline():
+#                 print(line_f1)
+#                 line_f2 = f2.readline()
+#                 assert line_f1 == line_f2
+
+
+# def dictsAlmostEqual(dict1, dict2, rel_tol=1e-3):
+#
+#     file1 = "/home/singha42/repos/gridappsd-testing/simulation_baseline_files/123-simulation.output"
+#
+#     file2 = "/tmp/output/simulation.output"
+#     with open(file1, 'r') as f1:
+#         with open(file2, 'r') as f2:
+#             # dict1 = [json.loads(line) for line in f1]
+#             # dict2 = [json.loads(line) for line in f2]
+#             dict1 = json.load(f1)
+#             dict2 = json.load(f2)
+#
+#             if len(dict1) != len(dict2):
+#                 return False
+#     #Loop through each item in the first dict and compare it to the second dict
+#
+#             for y in dict1:
+#                 m = dict1[y]
+#                 # If it is a nested dictionary, need to call the function again
+#                 if "magnitude" in m.keys():
+#                     for k in dict2:
+#                         n = dict2[k]
+#                         if not abs(m.get("magnitude") - n.get("magnitude") <= 1e-3):
+#                             return False
+#                 elif "angle" in m.keys():
+#                     if not abs(m.get("angle") - n.get("angle")) <= 0.1:
+#                         return False
+#                 elif "value" in m.keys():
+#                     if m.get("value") == n.get("value"):
+#                         return False
+#
+#             return True
 
 
 def test_start_gridappsd():
@@ -49,8 +87,9 @@ def test_start_gridappsd():
 
 
 @pytest.mark.parametrize("sim_config_file, sim_result_file", [
-    ("t1-p1-config.json", "t1-p1.output")
-    #, ("t2-p2-config.json", "t2-p1.output"),
+    #("9500-config.json", "9500-simulation.output")
+    #("123-config.json", "123-simulation.output"),
+    ("13-node-config.json", "13-node-sim.output"),
     # , ("t3-p1-config.json", "t3-p1.output"),
 ])
 def test_simulation_output(sim_config_file, sim_result_file):
@@ -63,12 +102,39 @@ def test_simulation_output(sim_config_file, sim_result_file):
     with startup_containers():
         with gappsd() as gapps:
             os.makedirs("/tmp/output", exist_ok=True)
-            with open("/tmp/output/foo.output", 'w') as outfp:
+            with open("/tmp/output/simulation.output", 'w') as outfile:
                 sim_complete = False
+                rcvd_measurement = False
 
                 def onmeasurement(sim, timestep, measurements):
-                    print(f"A measurement happened at {timestep}")
-                    outfp.write(f"{timestep}|{json.dumps(measurements)}\n")
+                    nonlocal rcvd_measurement
+                    #print(rcvd_measurement)
+                    if not rcvd_measurement:
+                        print(f"A measurement happened at {timestep}")
+                        outfile.write(f"{timestep}|{json.dumps(measurements)}\n")
+                        # data = {}
+                        # data["data"] = measurements
+                        # outfile.write(json.dumps(data))
+                        #rcvd_measurement = True
+
+                    print("Time is", int(time()))
+                    print("Start time is ", starttime)
+
+                    # if int(time()) > starttime + 10:
+                    #     sim.pause()
+                    #     # print(sim.simulation_id)
+                    #     outfile.write("\n Pausing simulation")
+
+                    # if int(time()) > starttime + 15:
+                    if timestep > starttime + 15:
+                        # print("Alka", starttime + 15)
+                        sim.resume_pause_at(starttime + 20)
+                        # print(sim.simulation_id)
+                        outfile.write("\n Resuming simulation")
+
+                    # if timestep == 1580420729:
+                    #     sim.resume()
+                    #     outfile.write("resuming")
 
                 def onfinishsimulation(sim):
                     nonlocal sim_complete
@@ -76,9 +142,13 @@ def test_simulation_output(sim_config_file, sim_result_file):
                     print("Completed simulator")
                 
                 print("Running config")
+                starttime = int(time())
                 # tm: added to get the simulation to run.  Copied from run_simulation.py.  Need to figure out what Craig was trying to do with the run_simulation code.
                 with open(sim_config_file) as fp:
                     run_config = json.load(fp)
+                    run_config["simulation_config"]["start_time"] = str(starttime)
+                    print(starttime)
+
                 sim = Simulation(gapps, run_config)
                 #sim = gapps.run_simulation(run_config)
 
@@ -88,7 +158,8 @@ def test_simulation_output(sim_config_file, sim_result_file):
                 print("Starting sim")
                 sim.start_simulation()
 
+
                 while not sim_complete:
                     sleep(5)
 
-            assert_files_are_equal(sim_result_file, "/tmp/output/foo.output")
+            #assert dictsAlmostEqual(sim_result_file, "/tmp/output/simulation.output")
