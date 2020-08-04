8218b541dacdb614ac97399c9217820fcef7e100
Craig Allwardt <craig.allwardt@pnnl.gov>
2020-08-03 20:19:40 -0400

    Fix for 9500 simulation logic

--- test_simulation_api.py
+++ test_simulation_api.py
@@ -14,30 +14,30 @@ from gridappsd import topics as t
 
 LOGGER = logging.getLogger(__name__)
 
+#
+# @contextmanager
+# def startup_containers(spec=None):
+#     LOGGER.info('Starting gridappsd containers')
+#     docker_up(spec)
+#     LOGGER.info('Containers started')
+#
+#     yield
+#
+#     LOGGER.info('Stopping gridappsd containers')
+#     docker_down()
+#     LOGGER.info('Containers stopped')
 
-@contextmanager
-def startup_containers(spec=None):
-    LOGGER.info('Starting gridappsd containers')
-    docker_up(spec)
-    LOGGER.info('Containers started')
 
-    yield
-
-    LOGGER.info('Stopping gridappsd containers')
-    docker_down()
-    LOGGER.info('Containers stopped')
-
-
-@contextmanager
-def gappsd() -> GridAPPSD:
-    gridappsd = GridAPPSD()
-    LOGGER.info('Gridappsd connected')
-
-    yield gridappsd
-
-    gridappsd.disconnect()
-    LOGGER.info('Gridappsd disconnected')
-
+# @contextmanager
+# def gappsd() -> GridAPPSD:
+#     gridappsd = GridAPPSD()
+#     LOGGER.info('Gridappsd connected')
+#
+#     yield gridappsd
+#
+#     gridappsd.disconnect()
+#     LOGGER.info('Gridappsd disconnected')
+#
 
 json_msg = []
 pause_msg = []
@@ -75,112 +75,122 @@ def on_message(self, message):
 @pytest.mark.parametrize("sim_config_file, sim_result_file", [
     ("9500-config.json", "9500-simulation.output")
     #("123-config.json", "123-simulation.output"),
-    #("13-new.json", "13-node-sim.output"),
+    #("13-node-config.json", "13-node-sim.output"),
 ])
-def test_simulation_output(sim_config_file, sim_result_file):
+def test_simulation_output(gridappsd_client, sim_config_file, sim_result_file):
     sim_config_file = os.path.join(os.path.dirname(__file__), f"simulation_config_files/{sim_config_file}")
     sim_result_file = os.path.join(os.path.dirname(__file__), f"simulation_baseline_files/{sim_result_file}")
     assert os.path.exists(sim_config_file), f"File {sim_config_file} must exist to run simulation test"
     # assert os.path.exists(sim_result_file), f"File {sim_result_file} must exist to run simulation test"
 
-    with startup_containers():
-        # Allow proven to come up
-        sleep(30)
-        #starttime = int(time())
+    gapps = gridappsd_client
 
-        with gappsd() as gapps:
-            os.makedirs("/tmp/output", exist_ok=True)
-            with open("/tmp/output/simulation.output", 'w') as outfile:
-                LOGGER.info('Configuring simulation')
-                sim_complete = False
-                rcvd_measurement = False
-                rcvd_first_measurement = 0
+    # Allow proven to come up
+    sleep(30)
+    #starttime = int(time())
+    os.makedirs("/tmp/output", exist_ok=True)
+    with open("/tmp/output/simulation.output", 'w') as outfile:
+        LOGGER.info('Configuring simulation')
+        sim_complete = False
+        rcvd_measurement = False
+        rcvd_first_measurement = 0
+        are_we_paused = False
+        received_measurment_count = 0
+
+        with open(sim_config_file) as fp:
+            LOGGER.info('Reading config')
+            run_config = json.load(fp)
+           # run_config["simulation_config"]["start_time"] = str(starttime)
+
+        sim = Simulation(gapps, run_config)
+
+        def onmeasurement(sim, timestep, measurements):
+            LOGGER.info('Measurement received at %s', timestep)
+            nonlocal rcvd_measurement
+            #nonlocal rcvd_first_measurement
+            #nonlocal are_we_paused
+            nonlocal received_measurment_count
+
+            received_measurment_count += 1
+            try:
+
+                # if not are_we_paused and not rcvd_first_measurement:
+                #     LOGGER.debug("Pausing sim now")
+                #     sim.pause()
+                #     are_we_paused = True
+                #     LOGGER.debug(f"WE ARE PAUSED {are_we_paused}")
+                #     # Setting up so if we get another measurement wheil we
+                #     # are paused we know it
+                #     rcvd_measurement = False
+
+                if not rcvd_measurement:
+                    print(f"A measurement happened at {timestep}")
+                    # outfile.write(f"{timestep}|{json.dumps(measurements)}\n")
+                    data = {"data": measurements}
+                    outfile.write(json.dumps(data))
+                    rcvd_measurement = True
+
+                else:
+                    rcvd_measurement = True
+                # rcvd_first_measurement = True
+            except Exception as ex:
+                LOGGER.error(f"An exception handled in callback {ex}")
+
+        # sleep here until rcvd_measuremnt = True again
+
+        def ontimestep(sim, timestep):
+            print("Timestamp: {}".format(timestep))
+
+        def onfinishsimulation(sim):
+            nonlocal sim_complete
+            sim_complete = True
+            LOGGER.info('Simulation Complete')
+
+       # LOGGER.info(f"Start time is {starttime}")
+        LOGGER.info('Loading config')
+
+        # tm: typo in add_onmesurement
+        LOGGER.info('sim.add_onmesurement_callback')
+
+        LOGGER.info('Starting sim')
+        sim.start_simulation()
+        print(sim.simulation_id)
+        sim.add_onmesurement_callback(onmeasurement)
+        sim.add_ontimestep_callback(ontimestep)
+        gapps.subscribe(t.simulation_log_topic(sim.simulation_id), on_message)
+        sim.add_oncomplete_callback(onfinishsimulation)
+        LOGGER.info('sim.add_oncomplete_callback')
+        secs = 0
+        num_measurements_before_pause = 3
+        while received_measurment_count < num_measurements_before_pause:
+            LOGGER.info(f"Waiting for at least {num_measurements_before_pause} measurements"
+                        f"but have {received_measurment_count} time take {secs}s ")
+            secs += 1
+            sleep(1)
+
+        LOGGER.debug("Pausing sim now")
+        sim.pause()
+        are_we_paused = True
+
+        paused_seconds = 0
+        while are_we_paused:
+            LOGGER.info(f"PAUSED {paused_seconds}")
+            paused_seconds += 1
+
+            # s
+            if paused_seconds > 30:
+                LOGGER.info('Resuming simulation')
+                sim.resume()
+                LOGGER.info('Resumed simulation')
                 are_we_paused = False
+                break
+            sleep(1)
 
-                with open(sim_config_file) as fp:
-                    LOGGER.info('Reading config')
-                    run_config = json.load(fp)
-                   # run_config["simulation_config"]["start_time"] = str(starttime)
+        assert not are_we_paused, "We should have came out of the paused_seconds > 30"
 
-                sim = Simulation(gapps, run_config)
-
-                def onmeasurement(sim, timestep, measurements):
-                    LOGGER.info('Measurement received at %s', timestep)
-                    nonlocal rcvd_measurement
-                    nonlocal rcvd_first_measurement
-                    nonlocal are_we_paused
-
-                    if not are_we_paused and not rcvd_first_measurement:
-                        LOGGER.debug("Pausing sim now")
-                        sim.pause()
-                        are_we_paused = True
-                        LOGGER.debug(f"WE ARE PAUSED {are_we_paused}")
-                        # Setting up so if we get another measurement wheil we
-                        # are paused we know it
-                        rcvd_measurement = False
-
-                    if not rcvd_measurement:
-                        print(f"A measurement happened at {timestep}")
-                        # outfile.write(f"{timestep}|{json.dumps(measurements)}\n")
-                        data = {"data": measurements}
-                        outfile.write(json.dumps(data))
-                        rcvd_measurement = True
-
-                    else:
-                        rcvd_measurement = True
-                    rcvd_first_measurement = True
-
-                # sleep here until rcvd_measuremnt = True again
-
-                def ontimestep(sim, timestep):
-                    print("Timestamp: {}".format(timestep))
-
-                def onfinishsimulation(sim):
-                    nonlocal sim_complete
-                    sim_complete = True
-                    LOGGER.info('Simulation Complete')
-
-               # LOGGER.info(f"Start time is {starttime}")
-                LOGGER.info('Loading config')
-
-                # tm: typo in add_onmesurement
-                LOGGER.info('sim.add_onmesurement_callback')
-
-                LOGGER.info('Starting sim')
-                sim.start_simulation()
-                print(sim.simulation_id)
-                sim.add_onmesurement_callback(onmeasurement)
-                sim.add_ontimestep_callback(ontimestep)
-                gapps.subscribe(t.simulation_log_topic(sim.simulation_id), on_message)
-                sim.add_oncomplete_callback(onfinishsimulation)
-                LOGGER.info('sim.add_oncomplete_callback')
-                secs = 0
-                while secs < 30:
-                    LOGGER.info(f"Sleep {secs}")
-                    # we have to wait until the first measurement is called before
-                    # the are_we_paused could  have a chance of being set
-                    secs += 1
-                    sleep(1)
-
-                paused_seconds = 0
-                while are_we_paused:
-                    LOGGER.info(f"PAUSED {paused_seconds}")
-                    paused_seconds += 1
-
-                    # s
-                    if paused_seconds > 120:
-                        LOGGER.info('Resuming simulation')
-                        sim.resume()
-                        LOGGER.info('Resumed simulation')
-                        are_we_paused = False
-                        break
-                    sleep(1)
-
-                assert not are_we_paused, "We should have came out of the paused_seconds > 30"
-
-                while not sim_complete:
-                    LOGGER.info('Sleeping')
-                    sleep(5)
+        while not sim_complete:
+            LOGGER.info('Sleeping')
+            sleep(5)
 
 
 def test_dictsEqual():
