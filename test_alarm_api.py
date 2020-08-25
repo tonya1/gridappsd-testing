8c6b558e48263903f181212132bc1d00383f85f1
Tonya Martin <tonya.martin@pnnl.gov>
2020-08-25 23:08:10 +0000

    Update mrids for latest release. Reduced duration and modified event times

--- test_alarm_api.py
+++ test_alarm_api.py
@@ -2,12 +2,11 @@ from contextlib import contextmanager
 import json
 import logging
 import os
-
+import yaml
 from time import sleep, time
 import sys
 import pytest
 
-import yaml
 from gridappsd import GridAPPSD
 from gridappsd.simulation import Simulation
 from gridappsd_docker import docker_up, docker_down
@@ -15,7 +14,7 @@ from gridappsd import topics as t
 
 LOGGER = logging.getLogger(__name__)
 
-tapchanger_value = -1
+tapchanger_value = []
 alarm_count = 0
 
 def on_message(headers, message):
@@ -34,24 +33,20 @@ def on_message(headers, message):
         measurement_values = message["message"]["measurements"]
         for x in measurement_values:
             m = measurement_values[x]
-            if m.get("measurement_mrid") == "_0f8202ca-a4bf-4c7e-9302-601919c09992":
-                if m.get("value") != tapchanger_value:
-                    if tapchanger_value == -1:
-                        LOGGER.info(f'Tap Changer value is {m.get("value")}')
-                    else:
-                        LOGGER.info(f'Tap Changer value changed from {tapchanger_value} to {m.get("value")} {m}')
-                    tapchanger_value = m.get("value")
-                    #with open("./tapchanger.txt", 'w+') as f:
-                    #    f.write('::::')
-                    #    f.write(str(m))
-
+            if m.get("measurement_mrid") == "_48e11ee1-ea9f-4e0c-a6dd-2807a9dbc032":
+               if not tapchanger_value:
+                    LOGGER.info(f'Tap Changer value is {m.get("value")}')
+                    tapchanger_value.append(m.get("value"))
+               else:
+                    if m.get("value") != tapchanger_value[-1]:
+                        LOGGER.info(f'Tap Changer value changed from {tapchanger_value[-1]} to {m.get("value")} {m}')
+                        tapchanger_value.append(m.get("value"))
 
 @pytest.mark.parametrize("sim_config_file, sim_result_file", [
     ("9500-alarm-config.json", "9500-alarm-simulation.output")
 
 ])
 def test_alarm_output(gridappsd_client, sim_config_file, sim_result_file):
-    #simulation_id = None
     sim_config_file = os.path.join(os.path.dirname(__file__), f"simulation_config_files/{sim_config_file}")
     sim_result_file = os.path.join(os.path.dirname(__file__), f"simulation_baseline_files/{sim_result_file}")
     assert os.path.exists(sim_config_file), f"File {sim_config_file} must exist to run simulation test"
@@ -60,57 +55,50 @@ def test_alarm_output(gridappsd_client, sim_config_file, sim_result_file):
     # Allow proven to come up
     sleep(30)
     
-    os.makedirs("/tmp/output", exist_ok=True)
-    with open("/tmp/output/simulation.output", 'w') as outfile:
-        sim_complete = False
-        rcvd_measurement = False
+    sim_complete = False
+    rcvd_measurement = False
 
-        def onmeasurement(sim, timestep, measurements):
-            nonlocal rcvd_measurement
-            # if not rcvd_measurement:
-            rcvd_measurement = True
-            #LOGGER.info('A measurement happened at %s', timestep)
+    
+    #def onmeasurement(sim, timestep, measurements):
+    #    nonlocal rcvd_measurement
+        # if not rcvd_measurement:
+    #    rcvd_measurement = True
+    #    LOGGER.info('A measurement happened at %s', timestep)
 
-        def onfinishsimulation(sim):
-            nonlocal sim_complete
-            sim_complete = True
-            LOGGER.info('Simulation Complete')
+    def onfinishsimulation(sim):
+        nonlocal sim_complete
+        sim_complete = True
+        LOGGER.info('Simulation Complete')
 
+    with open(sim_config_file) as fp:
+        LOGGER.info('Loading config')
+        run_config = json.load(fp)
+        LOGGER.info(f'Simulation start time {run_config["simulation_config"]["start_time"]}')
 
-        with open(sim_config_file) as fp:
-            LOGGER.info('Loading config')
-            run_config = json.load(fp)
-            LOGGER.info(f'Simulation start time {run_config["simulation_config"]["start_time"]}')
+    sim = Simulation(gapps, run_config)
 
-        sim = Simulation(gapps, run_config)
+    LOGGER.info('Starting the simulation')
+    sim.start_simulation()
 
-        LOGGER.info('Starting the simulation')
-        sim.start_simulation()
+    #LOGGER.info('sim.add_onmesurement_callback')
+    #sim.add_onmesurement_callback(onmeasurement)
+    LOGGER.info('sim.add_oncomplete_callback')
+    sim.add_oncomplete_callback(onfinishsimulation)
 
-        LOGGER.info('sim.add_onmesurement_callback')
-        sim.add_onmesurement_callback(onmeasurement)
-        LOGGER.info('sim.add_oncomplete_callback')
-        sim.add_oncomplete_callback(onfinishsimulation)
+    LOGGER.info("Querying for alarm topic")
+    alarms_topic = t.service_output_topic('gridappsd-alarms', sim.simulation_id)
+    log_topic = t.simulation_output_topic(sim.simulation_id)
+    gapps.subscribe(alarms_topic, on_message)
+    gapps.subscribe(log_topic, on_message)
 
-        LOGGER.info(f"Simulation id {sim.simulation_id}")
-        
-        LOGGER.info("Querying for alarm topic")
-        alarms_topic = t.service_output_topic('gridappsd-alarms', sim.simulation_id)
-        log_topic = t.simulation_output_topic(sim.simulation_id)
-        #input_topic = t.simulation_input_topic(sim.simulation_id)
-        gapps.subscribe(alarms_topic, on_message)
-        gapps.subscribe(log_topic, on_message)
-        # gapps.subscribe(input_topic, on_message)
-
-        while not sim_complete:
-            LOGGER.info('Sleeping')
-            sleep(30)
-
+    while not sim_complete:
+        LOGGER.info('Sleeping')
+        sleep(30)
 
 
 def test_tap_changer():
     global tapchanger_value
-    assert tapchanger_value == 10, f"Expected Tap Changer value at 10 received {tapchanger_value}"
+    assert tapchanger_value == [4, 10, 5], f"Expected tap changer values [4, 10, 5] received {tapchanger_value}"
 
 
 def test_alarm_count():
