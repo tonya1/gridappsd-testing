9e809a5c83f748aa93aa1b09b62acead7d34103a
Tonya Martin <tonya.martin@pnnl.gov>
2020-08-25 18:21:43 +0000

    Convert to gridappsd-python docker.  Change alarms mrid to equipment_name

--- test_alarm_api.py
+++ test_alarm_api.py
@@ -2,173 +2,117 @@ from contextlib import contextmanager
 import json
 import logging
 import os
+
 from time import sleep, time
 import sys
-import pandas as pd
 import pytest
-from numbers import Number
-from math import isclose
 
 import yaml
-from gridappsd import GridAPPSD, topics as t
-# tm: added for run_simulation workaround
+from gridappsd import GridAPPSD
 from gridappsd.simulation import Simulation
 from gridappsd_docker import docker_up, docker_down
+from gridappsd import topics as t
 
 LOGGER = logging.getLogger(__name__)
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-
-
-@contextmanager
-def startup_containers(spec=None):
-    LOGGER.info('Starting gridappsd containers')
-    docker_up(spec)
-    LOGGER.info('Containers started')
-
-    yield
-
-    LOGGER.info('Stopping gridappsd containers')
-    docker_down()
-    LOGGER.info('Containers stopped')
-
-
-@contextmanager
-def gappsd() -> GridAPPSD:
-    gridappsd = GridAPPSD()
-    LOGGER.info('Gridappsd connected')
-
-    yield gridappsd
-
-    gridappsd.disconnect()
-    LOGGER.info('Gridappsd disconnected')
-
-
-# def test_start_gridappsd():
-#    with startup_containers():
-#        g = GridAPPSD()
-#        assert g.connected
-# global tapchanger_value
 
 tapchanger_value = -1
 alarm_count = 0
-outage_mrid = ""
-count1 = 0
-count2 = 0
-
 
 def on_message(headers, message):
     global tapchanger_value
     global alarm_count
-    global outage_mrid
-    global count1
-    global count2
+    
     if "gridappsd-alarms" in headers["destination"]:
-        # print(headers)
-        if "_302E3119-B3ED-46A1-87D5-EBC8496357DF" or "_A0E0AB93-FFC2-471B-B84C-19015CB15ED2" or "_2FA4B41B-C31B-4861-B8BB-941A8DFD1B41" in \
-                message['equipment_mrid']:
+        if "ln1047pvfrm_sw" or "ln5001chp_sw" or "ln0895780_sw" in \
+                message['equipment_name']:
             for y in message:
-                print(y)
                 if "Open" in y['value']:
-                    LOGGER.info('Alarms created')
+                    LOGGER.info(f'Alarm created {y}')
                     alarm_count += 1
 
     if "gridappsd-alarms" not in headers["destination"]:
-        # print(headers)
         measurement_values = message["message"]["measurements"]
         for x in measurement_values:
-            # print(measurement_values)
             m = measurement_values[x]
             if m.get("measurement_mrid") == "_0f8202ca-a4bf-4c7e-9302-601919c09992":
                 if m.get("value") != tapchanger_value:
+                    if tapchanger_value == -1:
+                        LOGGER.info(f'Tap Changer value is {m.get("value")}')
+                    else:
+                        LOGGER.info(f'Tap Changer value changed from {tapchanger_value} to {m.get("value")} {m}')
                     tapchanger_value = m.get("value")
-
-                    LOGGER.info(f"Tap Changer Value changed to {tapchanger_value}")
-                count2 += 1
-
-
-            if m.get("measurement_mrid") == "_4b707748-2846-4517-92f6-fa6c3fbdd1ed":
-                count1 += 1
-
-            elif m.get("measurement_mrid") == outage_mrid:
-                outage_mrid = "_0f8202ca-a4bf-4c7e-9302-601919c09992"
-                print("mRID not present")
-
-            # if "_0f8202ca-a4bf-4c7e-9302-601919c09992" not in m.get("measurement_mrid"):
-            #     print("mRID not there")
-            #     with open("./input.txt", 'w') as f:
-            #         f.write(x)
-
-            # if m.get("measurement_mrid") == "_E44571D4-52CE-4ACE-9012-37DEBF17FCF8":
-            #     print("mRID not there")
-            #     with open("./input.txt", 'a') as f:
-            #         f.write(x)
+                    #with open("./tapchanger.txt", 'w+') as f:
+                    #    f.write('::::')
+                    #    f.write(str(m))
 
 
 @pytest.mark.parametrize("sim_config_file, sim_result_file", [
     ("9500-alarm-config.json", "9500-alarm-simulation.output")
 
 ])
-def test_alarm_output(sim_config_file, sim_result_file):
-
-    simulation_id = None
+def test_alarm_output(gridappsd_client, sim_config_file, sim_result_file):
+    #simulation_id = None
     sim_config_file = os.path.join(os.path.dirname(__file__), f"simulation_config_files/{sim_config_file}")
     sim_result_file = os.path.join(os.path.dirname(__file__), f"simulation_baseline_files/{sim_result_file}")
-
     assert os.path.exists(sim_config_file), f"File {sim_config_file} must exist to run simulation test"
 
-    with startup_containers():
-        with gappsd() as gapps:
-            os.makedirs("/tmp/output", exist_ok=True)
-            with open("/tmp/output/simulation.output", 'w') as outfile:
-                sim_complete = False
-                rcvd_measurement = False
+    gapps = gridappsd_client
+    # Allow proven to come up
+    sleep(30)
+    
+    os.makedirs("/tmp/output", exist_ok=True)
+    with open("/tmp/output/simulation.output", 'w') as outfile:
+        sim_complete = False
+        rcvd_measurement = False
 
-                def onmeasurement(sim, timestep, measurements):
-                    nonlocal rcvd_measurement
-                    # if not rcvd_measurement:
-                    rcvd_measurement = True
-                    LOGGER.info('A measurement happened at %s', timestep)
+        def onmeasurement(sim, timestep, measurements):
+            nonlocal rcvd_measurement
+            # if not rcvd_measurement:
+            rcvd_measurement = True
+            #LOGGER.info('A measurement happened at %s', timestep)
 
-                def onfinishsimulation(sim):
-                    nonlocal sim_complete
-                    sim_complete = True
-                    print("Completed simulator")
+        def onfinishsimulation(sim):
+            nonlocal sim_complete
+            sim_complete = True
+            LOGGER.info('Simulation Complete')
 
-                with open(sim_config_file) as fp:
-                    run_config = json.load(fp)
-                    print(run_config["simulation_config"]["start_time"])
 
-                sim = Simulation(gapps, run_config)
+        with open(sim_config_file) as fp:
+            LOGGER.info('Loading config')
+            run_config = json.load(fp)
+            LOGGER.info(f'Simulation start time {run_config["simulation_config"]["start_time"]}')
 
-                LOGGER.info('Starting the  simulation')
-                sim.add_onmesurement_callback(onmeasurement)
-                sim.add_oncomplete_callback(onfinishsimulation)
-                LOGGER.info('sim.add_onmesurement_callback')
-                LOGGER.info("Querying Alarm topic for alarms")
-                sim.start_simulation()
-                print(sim.simulation_id)
-                alarms_topic = t.service_output_topic('gridappsd-alarms', sim.simulation_id)
-                log_topic = t.simulation_output_topic(sim.simulation_id)
-                input_topic = t.simulation_input_topic(sim.simulation_id)
-                gapps.subscribe(alarms_topic, on_message)
-                gapps.subscribe(log_topic, on_message)
-                # gapps.subscribe(input_topic, on_message)
+        sim = Simulation(gapps, run_config)
 
-                while not sim_complete:
-                    LOGGER.info('Sleeping')
-                    sleep(30)
+        LOGGER.info('Starting the simulation')
+        sim.start_simulation()
+
+        LOGGER.info('sim.add_onmesurement_callback')
+        sim.add_onmesurement_callback(onmeasurement)
+        LOGGER.info('sim.add_oncomplete_callback')
+        sim.add_oncomplete_callback(onfinishsimulation)
+
+        LOGGER.info(f"Simulation id {sim.simulation_id}")
+        
+        LOGGER.info("Querying for alarm topic")
+        alarms_topic = t.service_output_topic('gridappsd-alarms', sim.simulation_id)
+        log_topic = t.simulation_output_topic(sim.simulation_id)
+        #input_topic = t.simulation_input_topic(sim.simulation_id)
+        gapps.subscribe(alarms_topic, on_message)
+        gapps.subscribe(log_topic, on_message)
+        # gapps.subscribe(input_topic, on_message)
+
+        while not sim_complete:
+            LOGGER.info('Sleeping')
+            sleep(30)
 
 
 
 def test_tap_changer():
     global tapchanger_value
-    global count1
-    global count2
-    print('A', count2)
-    print('B', count1)
-    assert tapchanger_value == 10, "Tap Changer value is not as expected"
+    assert tapchanger_value == 10, f"Expected Tap Changer value at 10 received {tapchanger_value}"
 
 
 def test_alarm_count():
     global alarm_count
-    assert alarm_count == 3, "Three Alarms were not generated"
+    assert alarm_count == 3, f"Expecting 3 alarms received {alarm_count}"
