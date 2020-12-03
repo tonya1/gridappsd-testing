a61ea81544667bca2a9ae2d9cc5e577ecf3ff22b
singha42 <alka.singh@pnnl.gov>
2020-12-02 19:39:36 -0500

    adding logging api test

--- test_logging_api.py
+++ test_logging_api.py
@@ -1,24 +1,14 @@
-from contextlib import contextmanager
 import json
 import logging
-
 import os
-
 from time import sleep, time
-import pytest
 import yaml
-
-from conftest import gridappsd_client
-from gridappsd import GridAPPSD
-
-# from gridappsd.simulation import Simulation
-# from gridappsd_docker import docker_up, docker_down
 from gridappsd import topics as t
 
 LOGGER = logging.getLogger(__name__)
 sim_id = "151989"
 log_topic = '/topic/goss.gridappsd.simulation.log'
-request_topic = '.'.join((t.BASE_SIMULATION_STATUS_TOPIC , sim_id))
+request_topic = t.simulation_log_topic(sim_id)
 
 
 os.environ['GRIDAPPSD_APPLICATION_ID'] = 'test-logging-service'
@@ -30,13 +20,19 @@ def on_message(self, message):
     json_msg = yaml.safe_load(str(message))
     print(json_msg)
 
-def test_logging_output(gridappsd_client):
 
+def test_logging_output(record_property, gridappsd_client):
+    doc_str = """This function queries the database through the gridappsd api.  Specifically checking that the 
+    specific logs are available.  The results are interrogated for the logs pushed to the topic and stored in the 
+    database. The return values of the query are interrogated and the values associated are tested """
+
+    record_property("gridappsd_doc", doc_str)
     gapps = gridappsd_client
-    gapps.subscribe(t.simulation_log_topic(sim_id), on_message)
     gapps_logger = gapps.get_logger()
     gapps_logger.info("Publishing logs to database")
-    query = {"query": "select * from log"}
+    sleep(5)
+    print("-------------")
+    query = {"query": "select * from log where log_message ='Publishing logs to database'"}
     print(gapps.get_response(t.LOGS, json.dumps(query), timeout=60))
     sleep(30)
 
